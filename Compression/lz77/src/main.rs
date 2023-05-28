83c883fbb37f1465547cc732026886595161c17f
Yuri <49888082+YuriRDev@users.noreply.github.com>
2023-05-27 21:59:01 -0300

    backup

--- Compression/lz77/src/main.rs
+++ Compression/lz77/src/main.rs
@@ -1,6 +1,24 @@
+use std::collections::HashMap;
+
+const WINDOW_SIZE: usize = 3;
+const TEXT: &str = "hello, my cute world! hello, my cute world!";
 fn main() {
-    const WINDOW_SIZE: i32 = 3;
+    let mut behind: HashMap<char, usize> = HashMap::new();
+    let mut count: usize = 0;
 
-    const TEXT: &str = "hello, my cute world! hello, my cute world!"; 
-
+    while count != TEXT.len() {
+        print!("Current char: ");
+        println!("{}", TEXT.bytes().nth(count).unwrap() as char);
+        print!("Current hash: ");
+        println!("{:?}", behind);
+        count += 1;
+    }
 }
+
+fn search_for_text(text: &str, map: HashMap<char, usize>) -> (usize, usize) {
+
+
+    (0, 0)
+}
+
+fn search_for_char() {}
