3d3699f5b6017c50f49e5d446b6db1298287686a
Yuri <49888082+YuriRDev@users.noreply.github.com>
2023-05-28 21:29:51 -0300

    `RLE` - Changes

--- Compression/RLE/src/lib.rs
+++ Compression/RLE/src/lib.rs
@@ -1,5 +1,36 @@
 /// Add docs later for compression
-pub fn compress_rle(text: &str) -> Vec<(u32, char)> {
+pub fn compress_rle(text: &str) -> String {
+    let mut count = 0;
+    let mut return_string = String::new();
+
+    while count != text.len() {
+        let current_char = text.bytes().nth(count).unwrap() as char;
+        let equals = count_equals(text, count);
+
+        // Did not find a better solution for this...
+        return_string += &format!("{}{}", equals, current_char);
+
+        count += equals as usize;
+    }
+
+    return_string
+}
+
+/// Add docs later for decompression
+fn decompres_rle(compressed: Vec<(u32, char)>) -> String {
+    let mut return_string = String::new();
+
+    for (quantity, character) in compressed {
+        for _b in 0..quantity {
+            return_string.push(character);
+        }
+    }
+
+    return_string
+}
+
+/// Add docs later for compression as vec
+fn compress_rle_as_vec(text: &str) -> Vec<(u32, char)> {
     let mut count = 0;
 
     let mut compressed: Vec<(u32, char)> = Vec::new();
@@ -16,8 +47,8 @@ pub fn compress_rle(text: &str) -> Vec<(u32, char)> {
     compressed
 }
 
-/// Add docs later for decompression
-pub fn decompres_rle(compressed: Vec<(u32, char)>) -> String {
+/// Add docs later for decompression from vec
+fn decompres_rle_from_vec(compressed: Vec<(u32, char)>) -> String {
     let mut return_string = String::new();
 
     for (quantity, character) in compressed {
@@ -50,46 +81,16 @@ mod test {
 
     #[test]
     fn test_compression_small() {
-        let answer: Vec<(u32, char)> = Vec::from([
-            (5, 'A'),
-            (6, 'B'),
-            (8, 'C'),
-            (2, 'D'),
-            (9, '0'),
-            (3, 'L'),
-            (1, 'B'),
-            (2, 'C'),
-        ]);
-        assert_eq!(answer, compress_rle("AAAAABBBBBBCCCCCCCCDD000000000LLLBCC"));
+
     }
 
     #[test]
     fn test_compression_medium() {
-        let answer: Vec<(u32, char)> = Vec::from([
-            (5, 'A'),
-            (6, 'B'),
-            (8, 'C'),
-            (2, 'D'),
-            (9, '0'),
-            (3, 'L'),
-            (1, 'B'),
-            (2, 'C'),
-        ]);
-        assert_eq!(answer, compress_rle("AAAAABBBBBBCCCCCCCCDD000000000LLLBCC"));
+
     }
 
     #[test]
     fn test_compression_big() {
-        let answer: Vec<(u32, char)> = Vec::from([
-            (5, 'A'),
-            (6, 'B'),
-            (8, 'C'),
-            (2, 'D'),
-            (9, '0'),
-            (3, 'L'),
-            (1, 'B'),
-            (2, 'C'),
-        ]);
-        assert_eq!(answer, compress_rle("AAAAABBBBBBCCCCCCCCDD000000000LLLBCC"));
+
     }
 }
--- Compression/RLE/src/main.rs
+++ Compression/RLE/src/main.rs
@@ -1,8 +1,9 @@
-use RLE::{compress_rle, decompres_rle};
+use RLE::compress_rle;
 
 fn main() {
     let text = "AAAAABBBBBBCCCCCCCCDD000000000LLLBCC";
 
     let compressed = compress_rle(text);
-    println!("{}", decompres_rle(compressed));
+    println!("{compressed}");
+    // println!("{}", decompres_rle(compressed));
 }
