bee38cc0ff17f3f1e1c895cebcad26fcd8e8a40a
Yuri <49888082+YuriRDev@users.noreply.github.com>
2023-05-28 21:02:48 -0300

    `RLE` - Decompression & Compression

--- Compression/RLE/src/lib.rs
+++ Compression/RLE/src/lib.rs
@@ -1,3 +1,4 @@
+/// Add docs later for compression
 pub fn compress_rle(text: &str) -> Vec<(u32, char)> {
     let mut count = 0;
 
@@ -15,6 +16,20 @@ pub fn compress_rle(text: &str) -> Vec<(u32, char)> {
     compressed
 }
 
+/// Add docs later for decompression
+pub fn decompres_rle(compressed: Vec<(u32, char)>) -> String {
+    let mut return_string = String::new();
+
+    for (quantity, character) in compressed {
+        for _b in 0..quantity {
+            return_string.push(character);
+        }
+    }
+
+    return_string
+}
+
+/// Add docs later for count_equal, not necessary, but why not
 fn count_equals(text: &str, count: usize) -> u32 {
     let current_char = text.bytes().nth(count).unwrap() as char;
 
@@ -34,7 +49,37 @@ mod test {
     use super::*;
 
     #[test]
-    fn small_test() {
+    fn test_compression_small() {
+        let answer: Vec<(u32, char)> = Vec::from([
+            (5, 'A'),
+            (6, 'B'),
+            (8, 'C'),
+            (2, 'D'),
+            (9, '0'),
+            (3, 'L'),
+            (1, 'B'),
+            (2, 'C'),
+        ]);
+        assert_eq!(answer, compress_rle("AAAAABBBBBBCCCCCCCCDD000000000LLLBCC"));
+    }
+
+    #[test]
+    fn test_compression_medium() {
+        let answer: Vec<(u32, char)> = Vec::from([
+            (5, 'A'),
+            (6, 'B'),
+            (8, 'C'),
+            (2, 'D'),
+            (9, '0'),
+            (3, 'L'),
+            (1, 'B'),
+            (2, 'C'),
+        ]);
+        assert_eq!(answer, compress_rle("AAAAABBBBBBCCCCCCCCDD000000000LLLBCC"));
+    }
+
+    #[test]
+    fn test_compression_big() {
         let answer: Vec<(u32, char)> = Vec::from([
             (5, 'A'),
             (6, 'B'),
--- Compression/RLE/src/main.rs
+++ Compression/RLE/src/main.rs
@@ -1,7 +1,8 @@
-use RLE::compress_rle;
+use RLE::{compress_rle, decompres_rle};
 
 fn main() {
     let text = "AAAAABBBBBBCCCCCCCCDD000000000LLLBCC";
 
-    println!("{:?}", compress_rle(text));
+    let compressed = compress_rle(text);
+    println!("{}", decompres_rle(compressed));
 }
