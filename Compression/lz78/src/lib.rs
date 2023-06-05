408c0a0402e303520438582d914f1965ed1ca582
Yuri <49888082+YuriRDev@users.noreply.github.com>
2023-06-05 19:41:42 -0300

    `Lz78` - Decompress (Dictionary length mut be < 10 for now)

--- Compression/lz78/src/lib.rs
+++ Compression/lz78/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, f64::RADIX};
 
 pub struct Lz78 {
     text: String,
@@ -84,7 +84,23 @@ impl Lz78 {
         compacted_string
     }
 
-    pub fn decompact(&self) -> Result<String, &str> {
+    /// ### Decompact text from Lz78 text.
+    /// Return the decompacted format as an error or a String.
+    ///
+    /// ## Warning
+    /// Input must be ```DecimalCharDecimalChar```
+    //
+    /// For now, dictionary must be length < 10, I was kinda dumb to do it that way
+    ///
+    /// ```
+    /// use lz78::Lz78;
+    ///
+    /// let mut decompress = Lz78::new("ABABCABCABCAABCAB");
+    /// let decompressed_text = compression.decompact();
+    ///
+    /// println!("{}", compacted_text);
+    /// ```
+    pub fn decompact(&mut self) -> Result<String, &str> {
         if validate_decompact_string(&self.text) == false {
             return Err(
                 "Invalid decompact string format \nMust be `number``char``number``char``...
@@ -93,10 +109,35 @@ impl Lz78 {
         }
 
         let mut decompact_str = String::new();
+        let mut cnt = 0;
+
+        while cnt < self.text.len() {
+            let pos = (self.text.as_bytes()[cnt] as char).to_digit(10).unwrap();
+            cnt += 1;
+
+            let value = self.text.as_bytes()[cnt] as char;
+            cnt += 1;
+
+            if pos == 0 {
+                self.add_prefix(&format!("{}", value));
+                decompact_str.push(value);
+            } else {
+                let mut tmp = String::new();
+                tmp.push_str(self.search_index(pos as usize).as_str());
+                tmp.push(value);
+
+                self.add_prefix(&tmp);
+                decompact_str.push_str(&tmp);
+            }
+        }
 
         Ok(decompact_str)
     }
 
+    /// ### Print's dictionary table
+    /// You can use this method after the ```.compact()``` or ```.decompact()``` method.
+    /// 
+    /// It'll will print the table in {:?} format.
     pub fn print_table(&self) {
         println!("{:?}", self.dictionary);
     }
@@ -113,6 +154,22 @@ impl Lz78 {
             Some(value) => Some(*value),
             None => None,
         }
+    }
+
+    /// Searches an map by it's value instead of the key
+    /// I'm pretty sure that exists more methods to do that.
+    /// But I couldn't find it at the time.
+    ///
+    /// ### Dev Note
+    /// I know I should have use an Option<String>, but to be honest, I'm lazy rn
+    fn search_index(&self, index: usize) -> String {
+        for (key, value) in &self.dictionary {
+            if *value + 1 == index {
+                return key.to_string();
+            }
+        }
+
+        return String::new();
     }
 
     /// Add a prefix on the hash_table
--- Compression/lz78/src/main.rs
+++ Compression/lz78/src/main.rs
@@ -1,7 +1,7 @@
 use lz78::Lz78;
 
 fn main() {
-    let mut compression = Lz78::new("1a0b0c0d0e0f0f0g03020103");
+    let mut compression = Lz78::new("0A0B1B0C3C5A6B");
     // let compacted_text = compression.compact();
     match compression.decompact() {
         Err(err) => println!("{err}"),
